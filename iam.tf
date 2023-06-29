4fa106f42eac1177f37018bafcc3b421587ec9ea
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2023-06-30 00:24:08 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -13,5 +13,5 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
 # attch policy
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
