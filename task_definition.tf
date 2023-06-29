6ff7f5fbd56653e1b3b3ea2be1623806ffe407f0
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2023-06-30 00:11:04 +0100

    Update task_definition.tf

--- task_definition.tf
+++ task_definition.tf
@@ -1,9 +1,9 @@
 resource "aws_ecs_task_definition" "flask_app_demo" {
   family                   = "flask-app-demo"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["EC2"]
-  cpu                      = "1"
-  memory                   = "128"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "2048"
   execution_role_arn = aws_iam_role.ecsTaskExecutionRole.id
 
   container_definitions = <<DEFINITION
@@ -14,8 +14,8 @@ resource "aws_ecs_task_definition" "flask_app_demo" {
         "essential": true,
         "portMappings": [
             {
-                "containerPort": 5000,
-                "hostPort": 5000
+                "containerPort": 80,
+                "hostPort": 80
             }
         ],
         "logConfiguration": {
